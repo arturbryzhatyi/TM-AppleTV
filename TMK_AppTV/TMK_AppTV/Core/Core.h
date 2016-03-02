0352a1e089f9bcac7c2e33c11fb61fad6d5ab9cb
Vitalii Obertynskyi <miraving@list.ru>
2016-03-02 13:55:53 +0200

    Connected pods

---
+++ TMK_AppTV/TMK_AppTV/Core/Core.h
@@ -0,0 +1,13 @@
+//
+//  Core.h
+//  TMK_AppTV
+//
+//  Created by Vitalii Obertynskyi on 3/2/16.
+//  Copyright © 2016 Vitalii Obertynskyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Core : NSObject
+
+@end
---
+++ TMK_AppTV/TMK_AppTV/Core/Core.m
@@ -0,0 +1,13 @@
+//
+//  Core.m
+//  TMK_AppTV
+//
+//  Created by Vitalii Obertynskyi on 3/2/16.
+//  Copyright © 2016 Vitalii Obertynskyi. All rights reserved.
+//
+
+#import "Core.h"
+
+@implementation Core
+
+@end
